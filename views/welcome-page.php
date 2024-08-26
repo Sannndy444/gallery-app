5f9c5804202e28af77b81c565bc15fee2a6e86c2
Sannndy444 <sandyriswantino1933@gmail.com>
2024-08-26 19:04:53 +0700

    first commit

---
+++ views/welcome-page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery App</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <div class="wrapper-welcome">
+        <div class="index-page">
+            <h2>Selamat Datang di Gallery App</h2>
+                <p><a href="views/login.php">Login</a> | <a href="views/signup.php">Signup</a></p>
+        </div>
+    </div>
+    
+</header>
